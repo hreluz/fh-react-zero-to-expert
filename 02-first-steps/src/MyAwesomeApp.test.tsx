16e2fe42e835431c9faafb96888429c3d8a587d0
Hector <hmreluz@gmail.com>
2025-08-15 16:38:59 -0500

    02-first-steps [TESTING]: search elements on render component

--- 02-first-steps/src/MyAwesomeApp.test.tsx
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -1,4 +1,4 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import {render, screen} from '@testing-library/react';
 import { MyAwesomeApp } from "./MyAwesomeApp";
 
@@ -6,7 +6,19 @@ describe('MyAwesomeApp', () => {
     test('should render firstName and lastName', () => {
         const {container} =render(<MyAwesomeApp/>)
 
-        screen.debug()
+        // screen.debug()
+        const h1 = container.querySelector('h1');
+        const h3 = container.querySelector('h3');
+        
+        expect(h1?.innerHTML).toContain('Bruce');
+        expect(h3?.innerHTML).toContain('Wayne');
+    });
 
+    test('should render firstName and lastName - screen', () => {
+        render(<MyAwesomeApp/>)
+
+        // screen.debug()
+        const h1 = screen.getByTestId('first-name-title');
+        expect(h1.innerHTML).toContain('Bruce');
     });
 });
