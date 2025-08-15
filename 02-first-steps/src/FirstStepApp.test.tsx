4fd97133f8d8253a118fcbbba0ec003003cb893f
Hector <hmreluz@gmail.com>
2025-08-15 17:36:12 -0500

    02-first-steps [TESTING]: test on FirstStepApp, mock compomnents

--- 02-first-steps/src/FirstStepApp.test.tsx
+++ 02-first-steps/src/FirstStepApp.test.tsx
@@ -1,6 +1,10 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
 import { render, screen } from "@testing-library/react";
+
+vi.mock('./shopping-cart/ItemCounter', () => ({
+    ItemCounter: () => <div data-testid="ItemCounter"/>
+}));
 
 describe('FirstStepApp', () => {
     test('should match snapshot', () => {
@@ -10,4 +14,14 @@ describe('FirstStepApp', () => {
 
         screen.debug()
     });
+
+    test('should render the correct number of ItemCounter compomnents', () => {
+        render(<FirstStepsApp/>)
+        
+        const itemCounters = screen.getAllByTestId('ItemCounter');
+
+        expect(itemCounters.length).toBe(4);
+
+        screen.debug();
+    })
 });
