34ac76938f2470e9547fe892471151e37d5df71a
Hector <hmreluz@gmail.com>
2025-08-15 15:22:07 -0500

    02-first-steps: useState

--- 02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 interface Props {
     name: string;
     quantity?: number;
 }
 
-export const ItemCounter = ({name, quantity}: Props) => {
+export const ItemCounter = ({name, quantity = 1}: Props) => {
+
+  const [counter, setCounter] = useState(quantity);
+
+  const handleAdd = () => {
+    setCounter(counter + 1);
+  }
+
+  const handleSubstract = () => {
+    if(counter == 1) return;
+    setCounter(counter - 1);
+  }
+
   return (
     <section style={{
         display: 'flex',
@@ -16,9 +30,9 @@ export const ItemCounter = ({name, quantity}: Props) => {
                 width: 150
             }}
         >{name}</span>
-        <button>+1</button>
-        <span>{quantity}</span>
-        <button>-1</button>
+        <button onClick={handleAdd}>+1</button>
+        <span>{counter}</span>
+        <button onClick={handleSubstract}>-1</button>
     </section>
   )
 }
