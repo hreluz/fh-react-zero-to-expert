d75418bca828b15de36326219c1f4a09458c4e02
Hector <hmreluz@gmail.com>
2025-08-15 12:32:10 -0500

    02-first-steps: print variables

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,21 @@
+const firstName = 'Bruce';
+const lastName  = 'Wayne';
+
+const favoriteGames = ['Batman Arkham Asylum', 'Batman Arkham City', 'Batman Arkham Knight'];
+
+const address = {
+    city: 'Miami',
+    state: 'California',
+    zipCode: 10788
+}
+
 export function MyAwesomeApp () {
     return (
         <>
-            <h1>Bruce</h1>
-            <h3>Wayne</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+            <p>{favoriteGames.join(', ')}</p>
+            <p>{JSON.stringify(address)}</p>
         </>
     )
 }
