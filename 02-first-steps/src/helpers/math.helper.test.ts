0fd9cf48bebde1dee9cfb81b1f2be2ac16df6c28
Hector <hmreluz@gmail.com>
2025-08-15 16:13:28 -0500

    02-first-steps [TESTING]: other tests

--- 02-first-steps/src/helpers/math.helper.test.ts
+++ 02-first-steps/src/helpers/math.helper.test.ts
@@ -1,13 +1,44 @@
-import {expect, test} from 'vitest';
-import { add } from './math.helpers';
+import {describe, expect, test} from 'vitest';
+import { add, multiply, subtract } from './math.helpers';
 
+describe('add', () => { 
+    test('should add two positives numbers', () => {
+        const a = 1;
+        const b = 2;
 
-test('should add two positives numbers', () => {
-    const a = 1;
-    const b = 2;
+        const result = add(a, b);
 
-    const result = add(a, b);
+        expect(result).toBe(a+b);
+    });
 
-    expect(result).toBe(a+b);
+    test('should add two negative positives numbers', () => {
+        const a = -1;
+        const b = -2;
 
+        const result = add(a, b);
+
+        expect(result).toBe(a+b);
+    });
+});
+
+describe('subtract', () => { 
+    test('should subtract two positives numbers', () => {
+        const a = 1;
+        const b = 2;
+
+        const result = subtract(a, b);
+
+        expect(result).toBe(a-b);
+    });
+});
+
+describe('mtultiply', () => { 
+    test('should mtultiply two positives numbers', () => {
+        const a = 3;
+        const b = 2;
+
+        const result = multiply(a, b);
+
+        expect(result).toBe(a*b);
+    });
 });
