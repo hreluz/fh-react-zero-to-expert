22df4abdc176272f3116ee2c95d5fdd7bb24e41b
Hector <hmreluz@gmail.com>
2025-08-16 02:31:59 -0500

    03-gifts-app: handle in cache

--- 03-gifts-app/src/counter/hooks/useGifs.tsx
+++ 03-gifts-app/src/counter/hooks/useGifs.tsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import { getGifsByQuery } from '../../gifs/actions/get-gifs-by-query.action';
 import type { Gif } from '../../gifs/interfaces/gifs.interfaces';
 
+const gifsCache: Record<string, Gif[]> = {};
+
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([])
 
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
   const handleTermClicked = async(term:string) => {
+    if (gifsCache[term]) {
+      setGifs(gifsCache[term]);
+      return ;
+    }
+
     const gifs = await getGifsByQuery(term);
     setGifs(gifs);
+
+    gifsCache[term] = gifs
   }
 
   const handleSearch = async(query:string = '') => {
